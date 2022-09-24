3e10a5900c9df05c15ae65d193c8164adda238bf
DarkbartoTM <93444335+DarkbartoTM@users.noreply.github.com>
2022-09-24 14:36:45 +0200

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Eric on 24/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
